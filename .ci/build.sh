4925fd60f56c6e3a66ac7c7bdc92ecfa971e436e
Lily Foster <lily@lily.flowers>
2020-09-08 20:45:23 -0400

    make first attempt at wav and m4a generation

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,13 +1,32 @@
 #!/bin/bash -ex
 shopt -s globstar
 
+rm -rf build
 rm -rf dist
+mkdir -p build
 mkdir -p dist
 
-for ly in ly/**/*.ly; do
-	name="${ly%.*}"
+curl -Lo build/MuseScore_General.sf3 'https://ftp.osuosl.org/pub/musescore/soundfont/MuseScore_General/MuseScore_General.sf3'
 
-	mkdir -p dist/"$name"
-	lilypond --pdf --png -d resolution=300 -o dist/"$name" -I "$(dirname "$(readlink -f "$ly")")" "$ly"
+for ly in ly/**/*.ly; do
+	path="${ly%.*}"
+	name="$(basename "$path")"
+
+	case "$ly" in
+		ly/carillon/*)
+			sf2='sf2/ClemsonCarillon.sf2'
+			;;
+
+		*)
+			sf2='build/MuseScore_General.sf3'
+			;;
+	esac
+
+	mkdir -p dist/"$path"
+
+	lilypond --pdf --png -d resolution=300 -o dist/"$path" -I "$(dirname "$(readlink -f "$ly")")" "$ly"
+
+	fluidsynth -ni "$sf2" dist/"$path"/"$name".midi -F dist/"$path"/"$name".wav
+	ffmpeg -i dist/"$path"/"$name".wav dist/"$path"/"$name".m4a
 done
 
