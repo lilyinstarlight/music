d7d6ef558a9d5f87940373c898f11d946cfda4a0
Lily Foster <lily@lily.flowers>
2020-09-11 18:21:03 -0400

    add m4a metadata to build

--- .ci/build.sh
+++ .ci/build.sh
@@ -27,6 +27,6 @@ for ly in ly/**/*.ly; do
 	lilypond --pdf --png -d resolution=300 -o dist/"$path" -I "$(dirname "$(readlink -f "$ly")")" "$ly"
 
 	fluidsynth -ni "$sf2" dist/"$path"/"$name".midi -F dist/"$path"/"$name".wav
-	ffmpeg -i dist/"$path"/"$name".wav dist/"$path"/"$name".m4a
+	ffmpeg -i dist/"$path"/"$name".wav -metadata title="$(grep '^\s\+title\s*=\s*.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata comment="$(grep '^\s\+instrument\s*=\s*.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata composer="$(grep '^\s\+composer\s*=\s*.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata artist="$(grep '^\s\+arranger\s*=\s*.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" dist/"$path"/"$name".m4a
 done
 
