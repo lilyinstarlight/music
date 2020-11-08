2bbcc003c6e27f090c685ebafde326b3e51e8d8d
Lily Foster <lily@lily.flowers>
2020-11-08 14:23:33 -0500

    ci: move to github actions from travis ci

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
-printenv SFTP_KEY | base64 -d >/tmp/sftp_rsa
-rclone sync --sftp-host "$SFTP_HOST" --sftp-user "$SFTP_USER" --sftp-key-file /tmp/sftp_rsa dist :sftp:"$SFTP_PATH"
+sftp_key="$(mktemp)"
+
+printenv SFTP_KEY | base64 -d >"$sftp_key"
+
+rclone sync --sftp-host "$SFTP_HOST" --sftp-user "$SFTP_USER" --sftp-key-file "$sftp_key" dist :sftp:"$SFTP_PATH"
