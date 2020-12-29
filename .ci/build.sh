607380b9ae89582cef3fc21806a87dee8302e739
Lily Foster <lily@lily.flowers>
2020-12-29 12:36:24 -0500

    ci: fix script tabbing

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,24 +9,24 @@ mkdir -p dist
 curl -Lo build/MuseScore_General.sf3 'https://ftp.osuosl.org/pub/musescore/soundfont/MuseScore_General/MuseScore_General.sf3'
 
 for ly in ly/**/*.ly wip/ly/**/*.ly; do
-	path="${ly%.*}"
-	name="$(basename "$path")"
+  path="${ly%.*}"
+  name="$(basename "$path")"
 
-	case "$ly" in
-		ly/carillon/*|wip/ly/carillon/*)
-			sf2='sf2/ClemsonCarillon.sf2'
-			;;
+  case "$ly" in
+    ly/carillon/*|wip/ly/carillon/*)
+      sf2='sf2/ClemsonCarillon.sf2'
+      ;;
 
-		*)
-			sf2='build/MuseScore_General.sf3'
-			;;
-	esac
+    *)
+      sf2='build/MuseScore_General.sf3'
+      ;;
+  esac
 
-	mkdir -p dist/"$path"
+  mkdir -p dist/"$path"
 
-	lilypond --pdf --png -d resolution=300 -o dist/"$path" -I "$(dirname "$(readlink -f "$ly")")" "$ly"
+  lilypond --pdf --png -d resolution=300 -o dist/"$path" -I "$(dirname "$(readlink -f "$ly")")" "$ly"
 
-	fluidsynth -ni "$sf2" dist/"$path"/"$name".midi -F dist/"$path"/"$name".wav
-	ffmpeg -i dist/"$path"/"$name".wav -metadata title="$(grep '^\s*title\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata comment="$(grep '^\s*instrument\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata composer="$(grep '^\s*composer\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata artist="$(grep '^\s*arranger\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" dist/"$path"/"$name".m4a
+  fluidsynth -ni "$sf2" dist/"$path"/"$name".midi -F dist/"$path"/"$name".wav
+  ffmpeg -i dist/"$path"/"$name".wav -metadata title="$(grep '^\s*title\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata comment="$(grep '^\s*instrument\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata composer="$(grep '^\s*composer\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" -metadata artist="$(grep '^\s*arranger\s*=.*$' "$ly" | sed -e 's/^[^"]*"\(.*\)"\s*$/\1/')" dist/"$path"/"$name".m4a
 done
 
